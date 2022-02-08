afd4de71eef056fcbfae1397de6a97b77291a950
manuelservin <servinchristianmanuel@gmail.com>
2022-02-08 08:21:33 -0300

    initial commit

---
+++ journal-app/src/routers/AppRouter.js
@@ -0,0 +1,29 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import AuthRouter from "./AuthRouter";
+
+import JournalScreen from "../components/journal/JournalScreen";
+
+const AppRouter = () => {
+  return (
+    <Router>
+      <div>
+        {/* A <Switch> looks through its children <Route>s and
+        renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/auth" component={AuthRouter} />
+
+          <Route exact path="/" component={JournalScreen} />
+          <Redirect to="/auth/login" />
+        </Switch>
+      </div>
+    </Router>
+  );
+};
+
+export default AppRouter;
